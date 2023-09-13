37b3af3bec410705c7302540cd1ba70eb4454ea3
Aulkan <capstone.graduation2023@gmail.com>
2023-09-13 19:40:41 +0800

    hgfht

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -17,7 +17,6 @@ const baseQuerywithReauth = async (args, api, extraOptions) => {
     let result  = await baseQuery(args, api, extraOptions)
 
     if(result?.error?.originalStatus === 403){
-        console.log('sending refresh token')
 
         const refreshResult = await baseQuery('/', api, extraOptions)
         console.log(refreshResult)
