b1a83d8a710a4977db3acd6286f71afabdebe2b3
Aulkanic <trazonamichael.pdm@gmail.com>
2024-03-07 14:24:05 +0800

    Revert "dasd"
    This reverts commit 366312849aea389b05c5ea816ae9937b17bd2e27.

--- src/Pages/Public/Home/home.jsx
+++ src/Pages/Public/Home/home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Bmccsite from '../../../LandingPage/lpages/Bmccsite'
-import CustomNavbar from '../../../Components/Navbar'
 
 export const Home = () => {
   return (
     <div>
-      <CustomNavbar />
+        <Bmccsite />
     </div>
   )
 }
